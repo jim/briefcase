3d6eab670de96868b2112d5482bd4b3bc42b0fc2
Jim Benton <jim@autonomousmachine.com>
2010-11-02 04:04:00 -0500

    Disabling functionality that doesn't exist yet

--- lib/shhh/main.rb
+++ lib/shhh/main.rb
@@ -16,8 +16,14 @@ command :sync do |c|
   c.when_called Shhh::Commands::Sync
 end
 
-command :generate do |c|
-  c.syntax = 'shhh generate'
-  c.description = 'Generates static versions of all erb dotfiles in ~/.dotfiles'
-  # c.when_called Shhh::Commands::Generate
-end
+# command :generate do |c|
+#   c.syntax = 'shhh generate'
+#   c.description = 'Generates static versions of all erb dotfiles in ~/.dotfiles'
+#   c.when_called Shhh::Commands::Generate
+# end
+
+# command :suggest do |c|
+#   c.syntax = 'shhh suggest'
+#   c.description 'List dotfiles that are in your home directory and not in ~/.dotfiles'
+#   c.when_called Shhh::Commands::Suggest
+# end
