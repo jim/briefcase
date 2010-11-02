67632b17c9ac4424938eeabf7e4765e668b696db
Jim Benton <jim@autonomousmachine.com>
2010-11-02 02:39:39 -0500

    Adding basic importing

--- lib/shhh/commands/base.rb
+++ lib/shhh/commands/base.rb
@@ -1,6 +1,12 @@
+require 'fileutils'
+
 module Shhh
   module Commands
     class Base
+      
+      include FileUtils
+      
+      DOTFILES_DIR = '.testfiles'
       
       def initialize(args, options)
         @args = args
@@ -11,10 +17,32 @@ module Shhh
       def execute
       end
     
+      def home_path
+        @home_path ||= `echo ~`.strip!
+      end
+      
+      def dotfiles_path
+        File.join(home_path, DOTFILES_DIR)
+      end
+      
+      def dotfile_path(file_path)
+        File.join(dotfiles_path, visible_name(file_path))
+      end
+      
+      def dotfile_exists?(file_path)
+        File.exist?(dotfile_path(file_path))
+      end
+    
+      def visible_name(file_path)
+        File.basename(file_path).gsub(/^\./, '')
+      end
+    
       def success(*args); say $terminal.color(format(*args), :green); end
       def info(*args); say $terminal.color(format(*args), :yellow); end
       def error(*args); say $terminal.color(format(*args), :red); end
       def warn(*args); say $terminal.color(format(*args), :yellow); end
+      
+      def fail(*args); say $terminal.color(format(*args), :red); exit; end
       
     end
   end
--- lib/shhh/commands/import.rb
+++ lib/shhh/commands/import.rb
@@ -3,7 +3,43 @@ module Shhh
     class Import < Base
       
       def execute
-        success("Importing %s", @args.first)
+        @path = File.expand_path(@args.first)
+        info("Importing %s to %s", @path, dotfiles_path)
+
+        fail("%s does not exist", @path) unless File.exist?(@path)
+
+        collision = dotfile_exists?(@path)
+
+        if !collision || overwrite_file?
+          mkdir_p(dotfiles_path)
+          destination = dotfile_path(@path)
+          
+          if collision
+            sideline = "#{destination}.old.#{Time.new.to_i}"
+            info "Renaming %s to %s", destination, sideline
+            mv(destination, sideline)
+          end
+          
+          mv(@path, destination)
+          info "Moving %s to %s", @path, destination
+
+          ln_s(destination, @path)
+          info "Symlinking %s to %s", @path, destination
+          
+          success 'Done'
+        else
+          info "Cancelled"
+        end
+        
+      end
+      
+      def overwrite_file?
+        decision = choose("#{@path} already exists as a dotfile. Do you want to replace it? Your original file will be renamed.", 'replace', 'skip') do |menu|
+          menu.index = :letter
+          menu.layout = :one_line
+        end
+        
+        decision == 'replace'
       end
       
     end
