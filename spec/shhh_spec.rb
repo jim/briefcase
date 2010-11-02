926830b63eed04a24cdc6cc0c9f6a0cce7981d87
Jim Benton <jim@autonomousmachine.com>
2010-11-02 11:16:24 -0500

    Using minitest for specs

---
+++ spec/shhh_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Shhh do
+  
+  describe Shhh::Commands::Import do
+
+    before do 
+      
+    end
+  
+    after do
+      cleanup_home_directory
+      cleanup_dotfile_directory
+    end
+  
+    it "should import a new dotfile" do
+    
+    
+    
+    end
+  
+  end
+  
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
 require 'rubygems'
-require 'test/unit'
-require 'shoulda'
+require 'minitest/spec'
+MiniTest::Unit.autorun
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'shhh'
-
-class Test::Unit::TestCase
-end
