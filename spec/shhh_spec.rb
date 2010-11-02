97c26a1b3b5e9f523a7ae4c884fac8db2a437b39
Jim Benton <jim@autonomousmachine.com>
2010-11-02 17:17:29 -0500

    Moving shared code into spec helper

--- spec/shhh_spec.rb
+++ spec/shhh_spec.rb
@@ -1,52 +1,6 @@
 require File.expand_path('spec_helper', File.dirname(__FILE__))
 require 'fileutils'
-require 'highline'
 include FileUtils
-
-def home_path
-  File.expand_path('spec_work/shhh_home', File.dirname(__FILE__))
-end
-
-def dotfiles_path
-  File.expand_path('spec_work/shhh_dotfiles', File.dirname(__FILE__))
-end
-
-def execute(command, *args)
-  options = args.last.is_a?(Hash) ? args.pop : {}
-  command_klass = "#{command.to_s.capitalize}"
-  terminal_out = nil
-  std_out, std_err = capture_io do
-    @output = Shhh::Commands.const_get(command_klass).new(args, options).say_buffer
-  end
-end
-
-def setup_work_directories
-  mkdir_p(home_path)
-  mkdir_p(dotfiles_path)
-  Shhh.home_path = home_path
-  Shhh.dotfiles_path = dotfiles_path
-end
-
-def cleanup_work_directories
-  rm_rf(home_path)
-  rm_rf(dotfiles_path)
-end
-
-$terminal = HighLine.new
-
-class Shhh::Commands::Base
-  attr_accessor :say_buffer
-  def say(message)
-    @say_buffer ||= []
-    @say_buffer << message.gsub(/\e\[\d+m/, '')
-  end
-end
-
-def output_must_contain(regex_or_string)
-  @output.any? do |line|
-    line =~ regex_or_string
-  end.must_equal(true, "Could not find #{regex_or_string} in: \n#{@output.join("\n")}")
-end
 
 describe Shhh do
   
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,52 @@
 require 'rubygems'
 require 'minitest/spec'
+require 'highline'
 MiniTest::Unit.autorun
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'shhh'
+
+def home_path
+  File.expand_path('spec_work/shhh_home', File.dirname(__FILE__))
+end
+
+def dotfiles_path
+  File.expand_path('spec_work/shhh_dotfiles', File.dirname(__FILE__))
+end
+
+def execute(command, *args)
+  options = args.last.is_a?(Hash) ? args.pop : {}
+  command_klass = "#{command.to_s.capitalize}"
+  terminal_out = nil
+  std_out, std_err = capture_io do
+    @output = Shhh::Commands.const_get(command_klass).new(args, options).say_buffer
+  end
+end
+
+def setup_work_directories
+  mkdir_p(home_path)
+  mkdir_p(dotfiles_path)
+  Shhh.home_path = home_path
+  Shhh.dotfiles_path = dotfiles_path
+end
+
+def cleanup_work_directories
+  rm_rf(home_path)
+  rm_rf(dotfiles_path)
+end
+
+$terminal = HighLine.new
+class Shhh::Commands::Base
+  attr_accessor :say_buffer
+  def say(message)
+    @say_buffer ||= []
+    @say_buffer << message.gsub(/\e\[\d+m/, '')
+  end
+end
+
+def output_must_contain(regex_or_string)
+  @output.any? do |line|
+    line =~ regex_or_string
+  end.must_equal(true, "Could not find #{regex_or_string} in: \n#{@output.join("\n")}")
+end
