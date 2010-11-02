d92825b1aba497dabd398233824b5cb04f62dbb6
Jim Benton <jim@autonomousmachine.com>
2010-11-02 01:29:20 -0500

    Disabling features that don't exist yet

--- lib/shhh/main.rb
+++ lib/shhh/main.rb
@@ -13,11 +13,11 @@ end
 command :link do |c|
   c.syntax = 'shhh link'
   c.description = 'Updates all symlinks for files included in ~/.dotfiles'
-  c.when_called Shhh::Commands::Link
+  # c.when_called Shhh::Commands::Link
 end
 
 command :generate do |c|
   c.syntax = 'shhh generate'
   c.description = 'Generates static versions of all erb dotfiles in ~/.dotfiles'
-  c.when_called Shhh::Commands::Generate
+  # c.when_called Shhh::Commands::Generate
 end
