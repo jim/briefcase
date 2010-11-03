05131f6b64c753b9e70c135a935fc436d3b30085
Jim Benton <jim@autonomousmachine.com>
2010-11-03 13:00:36 -0500

    Renaming varibles in import spec to make specs easier to understand

--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -4,6 +4,8 @@ describe Shhh::Commands::Import do
 
   before do
     setup_work_directories
+    @original_path = File.join(home_path, '.test')
+    @destination_path = File.join(dotfiles_path, 'test')
   end
 
   after do
@@ -16,86 +18,70 @@ describe Shhh::Commands::Import do
   end
   
   it "imports a dotfile" do
-    original = File.join(home_path, '.test')
-    moved = File.join(dotfiles_path, 'test')
-    
-    create_empty_file(original)
+    create_empty_file(@original_path)
 
-    run_command("import #{original}")
+    run_command("import #{@original_path}")
 
     output_must_contain(/Importing/, /Moving/)
-    file_must_exist(moved)
-    symlink_must_exist(original, moved)
+    file_must_have_moved(@original_path, @destination_path)
+    symlink_must_exist(@original_path, @destination_path)
   end
   
   it "imports a dynamic dotfile" do
-    original = File.join(home_path, '.test')
-    moved = File.join(dotfiles_path, 'test')
-    erb = File.join(dotfiles_path, 'test.erb')
-    
-    create_empty_file(original)
+    dynamic_path = File.join(dotfiles_path, 'test.erb')
+    create_empty_file(@original_path)
 
-    run_command("import #{original} --erb")
+    run_command("import #{@original_path} --erb")
 
     output_must_contain(/Importing/, /Moving/, /Creating ERB version at/)
-    file_must_exist(moved)
-    file_must_exist(erb)
-    symlink_must_exist(original, moved)
+    file_must_have_moved(@original_path, @destination_path)
+    symlink_must_exist(@original_path, @destination_path)
+    file_must_have_moved(@original_path, dynamic_path)
   end
   
   describe "collision handling" do
     
     before do
-      @original = File.join(home_path, '.test')
-      @moved = File.join(dotfiles_path, 'test')
-      @relocated = File.join(dotfiles_path, 'test.old.1')
-    
-      create_empty_file(@original)
-      create_empty_file(@moved)
-
+      @relocated_path = File.join(dotfiles_path, 'test.old.1')
+      create_empty_file(@original_path)
+      create_empty_file(@destination_path)
     end
     
     it "renames an existing dotfile when importing a duplicate and instructed to replace it" do
-    
-      run_command("import #{@original}") do |c|
+      run_command("import #{@original_path}") do |c|
         c.response(/Do you want to replace it\?/, 'replace')
       end
 
       output_must_contain(/Moving/, /Symlinking/, /already exists as a dotfile/)
-      file_must_exist(@moved)
-      file_must_exist(@relocated)
-      symlink_must_exist(@original, @moved)
+      file_must_exist(@destination_path)
+      file_must_exist(@relocated_path)
+      symlink_must_exist(@original_path, @destination_path)
     end
     
-    it "renames an existing duplicate dotfile  when importing a duplicate and instructed to replace it" do
-    
-      create_empty_file(@relocated)
-    
-      run_command("import #{@original}") do |c|
+    it "renames an existing duplicate dotfile when importing a duplicate and instructed to replace it" do
+      duplicate_path = File.join(dotfiles_path, 'test.old.2')
+      create_empty_file(@relocated_path)
+
+      run_command("import #{@original_path}") do |c|
         c.response(/Do you want to replace it\?/, 'replace')
       end
       
-      duplicate = File.join(dotfiles_path, 'test.old.2')
-      
-      file_must_have_moved(@moved, duplicate)
-      
-      file_must_exist(duplicate)
-      file_must_not_have_moved(@relocated)
+      file_must_have_moved(@destination_path, duplicate_path)
+      file_must_not_have_moved(@relocated_path)
     end
   
     it "does not modify an existing dotfile when instructed not to" do
-
-      run_command("import #{@original}") do |c|
+      run_command("import #{@original_path}") do |c|
         c.response(/Do you want to replace it\?/, 'skip')
       end
 
       output_must_contain(/already exists as a dotfile/)
       output_must_not_contain(/Moving/, /Symlinking/)
-      file_must_not_have_moved(@moved)
-      file_must_not_exist(@relocated)
-      symlink_must_not_exist(@original)
+      file_must_not_have_moved(@original_path)
+      file_must_not_have_moved(@destination_path)
+      file_must_not_exist(@relocated_path)
+      symlink_must_not_exist(@original_path)
     end
     
   end
-
 end
