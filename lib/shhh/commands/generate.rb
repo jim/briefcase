80b88473d546271f8ad01e7b14a6f2ba179ede68
Jim Benton <jim@autonomousmachine.com>
2010-11-23 00:22:56 -0600

    Added a warning to prevent accidental use

--- lib/shhh/commands/generate.rb
+++ lib/shhh/commands/generate.rb
@@ -6,33 +6,37 @@ module Shhh
         
         intro "Generating dynamic dotfiles in #{dotfiles_path}"
         
-        
+        secrets = read_secrets
         
         Dir.glob(File.join(dotfiles_path, '*.erb')) do |path|
-          basename = File.basename(path)
-          next if %w{. ..}.include?(basename)
+          basename = File.basename(path.gsub(/.erb$/, ''))
+          dotfile_path = generate_dotfile_path(basename)
           
-          if basename.include?('.old')
-            warn "Skipping %s, you may want to remove it.", path
+          if !File.exist?(dotfile_path) || overwrite_file?(dotfile_path)
+            
+            # generate statis file from erb
+            
           else
-            dotfile_name = ".#{basename}"
-            symlink_path = File.join(home_path, dotfile_name)
-            dotfile_path = generate_dotfile_path(basename)
-
-            if File.exist?(symlink_path)
-              if File.readlink(symlink_path) != dotfile_path
-                info "Removing outdated symlink %s", symlink_path
-                rm(symlink_path)
-                symlink(dotfile_path, symlink_path)
-              else
-                info "Symlink verified: %s -> %s", symlink_path, dotfile_path
-              end
-            else
-              symlink(dotfile_path, symlink_path)
-            end
+            
+            # skip it!
+            
           end
+          
+          
         end
+
         
+      end
+   
+   
+      # TODO consolidate this method and Import#overwrite_file?
+      def overwrite_file?(path)
+        decision = choose("#{path} already exists as a dotfile. Do you want to replace it?", 'replace', 'skip') do |menu|
+          menu.index = :letter
+          menu.layout = :one_line
+        end
+      
+        decision == 'replace'
       end
    
     end
