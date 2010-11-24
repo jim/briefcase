9cae280c680ce617aee4ef132fe944beed7cc067
Jim Benton <jim@autonomousmachine.com>
2010-11-24 00:25:11 -0600

    Refactoring the sync command

--- lib/shhh/commands/sync.rb
+++ lib/shhh/commands/sync.rb
@@ -3,7 +3,6 @@ module Shhh
     class Sync < Base
       
       def execute
-        
         intro "Synchronizing dotfiles between #{dotfiles_path} and #{home_path}"
         
         Dir.glob(File.join(dotfiles_path, '*')) do |path|
@@ -13,24 +12,29 @@ module Shhh
           if basename.include?('.old')
             warn "Skipping %s, you may want to remove it.", path
           else
-            dotfile_name = ".#{basename}"
-            symlink_path = File.join(home_path, dotfile_name)
-            dotfile_path = generate_dotfile_path(basename)
+            create_or_verify_symlink(basename)
+          end
+        end     
+      end
+      
+      private
+      
+      def create_or_verify_symlink(basename)
+        dotfile_name = ".#{basename}"
+        symlink_path = File.join(home_path, dotfile_name)
+        dotfile_path = generate_dotfile_path(basename)
 
-            if File.exist?(symlink_path)
-              if File.readlink(symlink_path) != dotfile_path
-                info "Removing outdated symlink %s", symlink_path
-                rm(symlink_path)
-                symlink(dotfile_path, symlink_path)
-              else
-                info "Symlink verified: %s -> %s", symlink_path, dotfile_path
-              end
-            else
-              symlink(dotfile_path, symlink_path)
-            end
+        if File.exist?(symlink_path)
+          if File.readlink(symlink_path) == dotfile_path
+            info "Symlink verified: %s -> %s", symlink_path, dotfile_path
+            return
+          else
+            info "Removing outdated symlink %s", symlink_path
+            rm(symlink_path)
           end
         end
         
+        symlink(dotfile_path, symlink_path)
       end
    
     end
